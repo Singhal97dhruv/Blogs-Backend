90e1fd776908864dea984067db5fd6ed4a8034dd
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-01 09:31:11 +0530

    Update imageController.js

--- controller/imageController.js
+++ controller/imageController.js
@@ -1,7 +1,7 @@
 import grid from 'gridfs-stream'
 import mongoose from 'mongoose';
 
-const url=process.env.PORT;
+const url="https://blogs-backend-singhal97dhruv.vercel.app/";
 
 const conn=mongoose.connection;
 
