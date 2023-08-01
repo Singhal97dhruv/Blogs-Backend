065ec7be3a18573d60c8599002fbc2e96bf382e0
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-01 16:42:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app=express();
 app.use(cors(
   {
     origin: ["https://blog-kabootar.vercel.app"],
-    methods: ["POST","GET"],
+    methods: ["POST","GET","PUT","DELETE"],
     credentials: true
   }
 ));
